f5422cfe4146a31e905667b5bf103de661a20a9f
grgc25 <grgc25@gmail.com>
2015-02-15 19:52:05 -0600

    Create if-elif-else.py

---
+++ if-elif-else.py
@@ -0,0 +1,11 @@
+print ("que OS usas en tu telefono?")
+ans=raw_input("-> ")
+
+if ans=="android":
+  print("chido")
+elif ans=="ios":
+  print("BOOOM")
+elif ans=="rim":
+  print("enserio??")
+else:
+  print("ANDROID MASTER RACE!")
