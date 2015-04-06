5b36ef0d28b12c96d053a58be1eba1df74b85e65
grgc25 <grgc25@gmail.com>
2015-04-05 20:38:38 -0500

    Create WSQ12_babilonian.py

---
+++ WSQ12_babilonian.py
@@ -0,0 +1,12 @@
+def raiz(n):
+ r1=n #la formula del metodo babilonico nos pide intentar adivinar la raiz pero pondremos el mismo numero por poner algo
+ c=0
+ while(c<10): #entre mas veces se repita el proceso, sera mas acertado. haremos que se repita 10 veces.
+   raiz= (r1+(n/r1))/2 #Es la formula del metodo babilonico
+   r1=raiz #el numero que suponemos sera cambiado por el resultado de la formula cada vez que se repita la operacion para hacer el resultado mas preciso
+   c=c+1 #parte del contador
+ return raiz
+ 
+n=float(input("De que numero quieres sacar su raiz cuadrada? -> "))
+respuesta=raiz(n)
+print("la raiz de ",n," es ",respuesta,)
